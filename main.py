ef15187c2395c554365b48c72bb6aab161d8ab2d
Iooodo <iodonadin@mail.ru>
2024-04-24 19:47:16 +0300

    add new print

--- main.py
+++ main.py
@@ -1,4 +1,143 @@
-def foo():
-    foo()
-class Bar(object):
-    _slots_ = ()
+#a = 89.9889
+#print (pow(-4, 0.5))
+
+
+
+#a = -5.9979
+#print(abs(a))
+
+#a = 0.9
+#print(a.as_integer_ratio())
+
+#a = int(7.998948)
+#print(a)
+
+#a = int("123450", 6)
+#print(a)
+
+#a = float("-.1090_889_1")
+#print(a)
+
+#print(float("inf"))
+#print(float("-inf"))
+#print(float("nan"))
+#print(float("-nan"))
+
+
+#a = (473487+2j)
+#print(a)
+
+#[-5, 256]
+
+#a = 4
+#b = 2*2
+#print(a is b)
+
+#a = None           всегда False
+#print(a is None)   только через is
+
+#print(2 == 2.0)
+#print(2 != 7)
+#print(2 > 5)
+#print(2 <= 9)
+#print(2 is not 2.0)
+
+#text = " "         true
+#text = ""          false
+
+#text = "abckjdl"
+#print(text[-3])
+#print(text[0])
+#print(text[1])
+#print(text[:: -1])
+#print(text[1:5:1])
+#print(text[::2])
+#print(text[slice(0, 5, 2)])
+#print(len(text))
+
+#a = "hello"
+#b = "hello"
+#print(a == b)
+
+#a = "helLo"
+#b = "hello"
+#print(a > b)
+
+#a = "hello world"
+#b = "hello world"
+#print(a is b)
+
+#a = "hello world"
+#print("hell" not in a)
+
+#a = "hello"
+#b = "world"
+#c = a + b
+#print(c * 3)
+
+#a = "hello\n\nworld"
+#print(a)
+
+#a = r"hello\n\nworld"
+#print(a)
+
+#name = "Alex"
+#email = "alex@mail.ru"
+#age = 34
+#is_human = True
+#text = f"Hello {name} your email {email}"
+#print(text)
+
+#text = f"{34545:,}"
+#print(text)
+
+#text = f"{34545:.3f}"
+#print(text)
+
+#text = f"{34545:.3f} \N{fire}"
+#print(text)
+
+#text = "HELLO world"
+#print(text.capitalize())
+#print(text.lower())
+#print(text.title())
+#print(text.casefold())         сворачивает лигатуру
+#print(text.islower())
+#print(text.isupper())
+#print(text.isalpha())
+#print(text.isdigit())
+#print(text.isalnum())
+#print(text.isprintable())
+#print(text.isascii())
+#print(text.isidentifier())
+
+
+
+#text = "Hello world"
+#print(text.count("ell",2,7))
+
+#print("hello". endswith("ello"))
+#print("hello". startswith("ello"))
+
+#print("Hello Hello".find("el"))
+
+#print("Hello World Python".split(" "))
+#print("Hello World Python".split(sep:"--", maxsplit: 1))
+
+#text = "  \t Hello world   \t"
+#print(text.lstrip())
+#print(text.rstrip())
+#print(text.strip())
+
+#print(ord("\n"))
+#print(chr(0))
+
+
+
+#print(bin(15))
+#print(bin(9))
+#print(bin(15 & 9))
+#print(bin(15 | 9))
+#print(bin(15 ^ 9))
+#print(~-4)          инверсивное значение   (+1 и *-1)
+#branch homework            дз
