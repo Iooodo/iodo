d81a8d9d1f1367609ccda66073f355ed4430f083
Iooodo <iodonadin@mail.ru>
2024-04-26 20:39:07 +0300

    l3

--- HOMEWORK/3_2.py
+++ HOMEWORK/3_2.py
@@ -1,8 +1,11 @@
 #Пользователь вводит 3 числа, найти среднее арифмитическое с
 #точность 3
 
-number1 = input('')
-number2 =
-number3 =
-number_2 = sum(numbers) / len(numbers)
-print(round(number_2, 3))
+number1 = int(input('Enter number: '))
+number2 = int(input('Enter number: '))
+number3 = int(input('Enter number: '))
+sum_numbers = number1 + number2 + number3               #7
+numbers = ('number1', 'number2', 'number3')
+amt_numbers = len(numbers)
+print(round((sum_numbers / amt_numbers), 3))
+
