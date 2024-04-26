d28541142ebe34039b6857de38bb6159d9a41a56
Iooodo <iodonadin@mail.ru>
2024-04-26 18:59:46 +0300

    l3

---
+++ HOMEWORK/3_1.py
@@ -0,0 +1,12 @@
+# Пользователь вводит предложение, заменить все пробелы на "-" 2-мя
+# способами
+
+sentense = "I find some interesting information"
+print(sentense.replace(' ', '-'))
+
+word1 = "I"
+word2 = "find"
+word3 = "some"
+word4 = "interesting"
+word5 = "information"
+print(word1, word2, word3, word4, word5, sep='-')
