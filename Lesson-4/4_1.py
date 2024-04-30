40bc75d4ec2dcf92cd90b45ed70ea3ebd52b50bf
Iooodo <iodonadin@mail.ru>
2024-04-30 19:05:35 +0300

    l3

--- Lesson-4/4_1.py
+++ Lesson-4/4_1.py
@@ -1,6 +1,10 @@
 # Заполнить список степенями числа 2 (от 2^1 до 2^n).
 
+#
+# n = int(input("n="))
+# list = [2**i for i in range(1, n+1)]
+# print(list)
 
 n = int(input("n="))
-list = [2**i for i in range(1, n+1)]
+list = [pow(2, i) for i in range(1, n+1)]
 print(list)
