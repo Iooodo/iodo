f6528a1a4523889bce06d0e30b0e3d403e269162
Iooodo <iodonadin@mail.ru>
2024-05-01 15:03:10 +0300

    l3

---
+++ Lesson-5/5_3.py
@@ -0,0 +1,11 @@
+# Вывести четные числа от 2 до N по 5 в строку
+
+n = int(input("n="))
+
+# for number in range(2, n+1):
+#     if number % 2 == 0:
+#         print(number, end='\n' if number == n else ", " if (number % 5) else ',\n')
+
+for number in range(2, n+1):
+    if number % 2 == 0:
+        print(number, end=' ' if number % 5 else '\n')
