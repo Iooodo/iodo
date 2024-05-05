29d2aa1916f0591fc6f3bad1db5ab2af069508c0
Iooodo <iodonadin@mail.ru>
2024-05-05 15:28:57 +0300

    l3

---
+++ Lesson-6/6_3.py
@@ -0,0 +1,18 @@
+# Дан список чисел и на вход поступает число N, необходимо сместить список на
+# указанное число, пример: [1,2,3,4,5,6,7] N=3 ответ: [5,6,7,1,2,3,4]
+
+def smpl_func(lst, steps):
+    if steps < 0:
+        steps = abs(steps)
+        for i in range(steps):
+            lst.append(lst.pop(0))
+    else:
+        for i in range(steps):
+            lst.insert(0, lst.pop())
+
+n = int(input('N='))
+numbers = [1,2,3,4,5,6,7]
+
+smpl_func(numbers, n)
+print(numbers)
+
