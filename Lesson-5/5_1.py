44dbc7b5563280f58f17d0da0f91ca2fb23c092b
Iooodo <iodonadin@mail.ru>
2024-05-05 12:45:55 +0300

    l3

--- Lesson-5/5_1.py
+++ Lesson-5/5_1.py
@@ -1,18 +1,15 @@
 # Вывести первые N чисел кратные M и больше K
 # n=5 m=2 k=2           2 4 6 8 10
 
-# n = int(input("n="))
-# m = int(input("m="))
-# k = int(input("k="))
-#
-# for number in range(n+1):
-#     if number % m == 0 and number > k:
-#         print(number)
-K +=1
-while N:
-    if not K % M:
-        print(K)
-        N -= 1
-        K += M
+n = int(input("n="))
+m = int(input("m="))
+k = int(input("k="))
+
+k +=1
+while n:
+    if not k % m:
+        print(k)
+        n -= 1
+        k += m
     else:
-        K += 1
+        k += 1
