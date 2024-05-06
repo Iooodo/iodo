2dbf3d5f77a6032dc844bde83864596366b9c233
Iooodo <iodonadin@mail.ru>
2024-05-06 20:10:16 +0300

    l3

--- Lesson-6/6_9.py
+++ Lesson-6/6_9.py
@@ -3,13 +3,17 @@
 # имена тех, у кого не указана почта (нет ключа email или значение этого ключа -
 # пустая строка)
 
-members = {
+persons = {
     "000123": dict([("name", "Nadya"), ("s_name", "Iodo"), ("phone_number", "+296680968"), ("email", "iodonadin@mail.ru")]),
     "000124": dict([("name", "Katya"), ("s_name", "Kurts"), ("phone_number", "+296201029"), ("email", "")]),
-    "000125": dict([("name", "Nastya"), ("s_name", "Dashko"), ("phone_number", "+293201888"), ("adres", "iodonadin@mail.ru")])
+    "000125": dict([("name", "Nastya"), ("s_name", "Dashko"), ("phone_number", "+293201888"), ("city", "Minsk")])
 }
-city_input = "email"
 
-for country, towns in members.items():
-    if city_input in towns and city_input is not None:
-        print(f'{towns} .')
+values = list(persons.values())
+
+def no_em(l):
+    for person, inf in enumerate(l):
+        if inf.get('email', '') == '':
+            print(inf.get('name'))
+
+no_em(values)
