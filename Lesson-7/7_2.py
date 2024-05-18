0178ecdd07bafa27c9fc0fd080bbaf620261e0fc
Iooodo <iodonadin@mail.ru>
2024-05-18 17:21:18 +0300

    l3

--- Lesson-7/7_2.py
+++ Lesson-7/7_2.py
@@ -20,17 +20,41 @@ class Car:
         return (f'Car information: color - {self.color}, number of passenger seats - {self.count_passenger_seats}, '
                 f'baby seat - {self.is_baby_seat}, busy - {self.is_busy}')
 
-
 car_1 = Car('red', 5, True)
 car_2 = Car('blue', 2, False)
 car_3 = Car('green', 4, True)
 car_4 = Car('yellow', 5, False)
 
-
 class Taxi:
     def __init__(self):
-        self.cars = list[Car]
-    def find_car(self, count_passengers: int):
-       if 
+        self.cars = [car_1, car_2, car_3, car_4]
+    def find_car(self, count_passengers: int, is_baby: bool):
+       for car in self.cars:
+           if car.count_passenger_seats >= count_passengers and car.is_baby_seat == is_baby and car.is_busy is False:
+               print(car)
+               car.is_busy = True
+               break
+           elif car.count_passenger_seats >= count_passengers and is_baby is False and car.is_busy is False:
+               print(car)
+               car.is_busy = True
+               break
+       else:
+           if car.count_passenger_seats < count_passengers:
+               print(None)
+           elif car.is_busy is True:
+               print(None)
 
+
+order1 = Taxi()
+order1.find_car(5, True)
+order2 = Taxi()
+order2.find_car(5, False)
+order3 = Taxi()
+order3.find_car(2, True)
+order4 = Taxi()
+order4.find_car(7, False)
+order5 = Taxi()
+order5.find_car(1, False)
+order6 = Taxi()
+order6.find_car(2, False)
 
