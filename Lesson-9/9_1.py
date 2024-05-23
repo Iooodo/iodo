2af1581f8fead80b57a744014f94bc78460c0d3f
Iooodo <iodonadin@mail.ru>
2024-05-23 19:02:26 +0300

    l3

---
+++ Lesson-9/9_1.py
@@ -0,0 +1,61 @@
+# Дан файл с многострочным текстом, необходимо подсчитать количество букв в каждой строке и результат записать в другой файл в формате:
+# 1 строка - N букв
+# 2 строка - M букв
+
+# file = open('file-1', 'r', encoding='utf-8')
+# text = file.readlines()
+# # print(len(text[3])
+# line_str = len(text[0])
+# file_2 = open('file-2.txt', 'w', encoding='utf-8')
+# file_2.write(f'1 строка - {line_str} букв')
+
+# with open('file-1', 'r', encoding='utf-8') as file_1:
+#     with open('file-2.txt', 'w', encoding='utf-8') as file_2:
+#         for line in file_1:
+#             line = line.strip()
+#             num_let = len(line)
+#             print(num_let)
+            # text = file_1.readlines()
+            # print(num_let)
+            # for i in range(1, len(text)+2):
+            #     # text = file_1.readlines()
+            #     print(i, 'line -', num_let, 'letters')
+        # for line in file_1:
+            # file_2.write()
+            # print(text.index(), 'line -', num_let, 'letters')
+
+
+#
+# with open('file-2.txt', 'w', encoding='utf-8') as file_2:
+#     for line in file_1:
+#         print(line.index(), 'строка -', num_let, 'букв')
+#
+# file = open('file-1', 'r', encoding='utf-8')
+# text = file.readlines()
+# # print(len(text[3])
+# line_str = len(text[0])
+# count_lines = len(text)
+# # print(count_lines)
+# file_2 = open('file-2.txt', 'w', encoding='utf-8')
+# # file_2.write(f'1 строка - {line_str} букв')
+# for i in range(0, count_lines):
+
+# with open('file-1', 'r', encoding='utf-8') as file_1:
+#     with open('file-2.txt', 'w', encoding='utf-8') as file_2:
+#         for line in file_1:
+#             line = line.strip()
+#             text = file_1.readlines()
+#             num_let = len(text)
+#             print(text, num_let)
+            # print(num_let)
+            # for i in range(1, len(text)+2):
+            #     # text = file_1.readlines()
+            #     print(i, 'line -', num_let, 'letters')
+#
+# with open('file-1', 'r', encoding='utf-8') as file_1:
+#     text = file_1.readlines()
+#         for line in file_1:
+#             line = line.strip()
+#             text = file_1.readlines()
+#             num_let = len(text)
+#             print(text, num_let)
