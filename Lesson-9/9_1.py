9eeded6fc637f307ae3be5db1fc26683a6f03cd9
Iooodo <iodonadin@mail.ru>
2024-05-25 15:53:49 +0300

    l3

--- Lesson-9/9_1.py
+++ Lesson-9/9_1.py
@@ -3,13 +3,17 @@
 # 2 строка - M букв
 
 
+class TextWriter:
 
+    def writer(self, name_file1: str, name_file2: str):
+        counter = 1
+        with (
+            open(name_file1, 'r', encoding='utf-8') as file_1,
+            open(name_file2, 'w', encoding='utf-8') as file_2
+        ):
+                for line in file_1:
+                    file_2.write('{} строка - {} букв\n'.format(counter, len(line)-1))
+                    counter += 1
 
-counter = 1
-with (
-    open('file-1', 'r', encoding='utf-8') as file_1,
-    open('file-2.txt', 'w', encoding='utf-8') as file_2
-):
-        for line in file_1:
-            file_2.write('{} строка - {} букв\n'.format(counter, len(line)-1))
-            counter += 1
+a = TextWriter()
+a.writer('file-1', 'file-2.txt')
