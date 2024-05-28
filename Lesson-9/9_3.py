5122505c0fab90a2e6c3065cca326b90a36881a0
Iooodo <iodonadin@mail.ru>
2024-05-28 19:09:28 +0300

    l3

--- Lesson-9/9_3.py
+++ Lesson-9/9_3.py
@@ -3,3 +3,12 @@
 # В файле tasks находится список задач и подзадач каждой задачи.
 # 	По название Билда, необходимо вывести список задач и подзадач для выполнения билда (задачу нельзя выполнить до выполнения подзадач).
 
+import yaml
+
+with open('D:/Python обучение/Lessons/builds.yaml') as fh:
+    read_data = yaml.load(fh, Loader=yaml.FullLoader)
+print(read_data)
+
+with open('D:/Python обучение/Lessons/tasks.yaml') as fh:
+    read_dat = yaml.load(fh, Loader=yaml.FullLoader)
+# print(read_dat)
