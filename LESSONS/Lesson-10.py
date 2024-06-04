4d9879b4e5f9d960f698c9c3620d109731b2da6c
Iooodo <iodonadin@mail.ru>
2024-06-04 19:02:25 +0300

    l3

--- LESSONS/Lesson-10.py
+++ LESSONS/Lesson-10.py
@@ -1,2 +1,38 @@
-from pydantic import BaseModel
+from os import *
+from pathlib import *
+from shutil import *
+from datetime import *
 
+
+
+
+# makedirs(name= "LESSONS/les-10")
+# remove("LESSONS/les-10")
+
+# p = Path(__file__).resolve().parent.parent.parent
+# print(p)
+
+
+# d = datetime.now()
+# print(d)
+
+#  d = datetime.utsnow()            # НЕ ИСПОЛЬЗОВАТЬ - ОН DEPRECATED
+# print(d)
+
+# d = datetime.now(tz=UTC)
+# print(d)
+# print(d.timestamp())
+# print(d.fromtimestamp(1717245807.845073, tz=UTC))
+
+
+# print(__name__)
+
+# from pydantic import BaseModel, EmailStr
+# from pydantic_extra_types.
+#
+#
+# class Valid(BaseModel):
+#     email: EmailStr
+#
+# data = Valid(email="iodonadin@mail.ru")
+# print(data)
