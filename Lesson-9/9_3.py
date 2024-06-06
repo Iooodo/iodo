a986573e144395a4a21c63bc404a835bdcacb62f
Iooodo <iodonadin@mail.ru>
2024-06-06 18:58:51 +0300

    l3

--- Lesson-9/9_3.py
+++ Lesson-9/9_3.py
@@ -3,12 +3,33 @@
 # В файле tasks находится список задач и подзадач каждой задачи.
 # 	По название Билда, необходимо вывести список задач и подзадач для выполнения билда (задачу нельзя выполнить до выполнения подзадач).
 
-import yaml
+def task_subtasks(
+        task_name: str,
+        tasks: dict[str, list[dict[str, str | list[str]]]]
+) -> list[str]:
+    data = []
+    for task in tasks.get("tasks"):  # type: dict
+        if task.get("name") == task_name:
+            for subtask in task.get("dependencies"):
+                data.extend(
+                    task_subtasks(task_name=subtask, tasks=tasks)
+                )
+            data.append(task_name)
+            break
+    return data
 
-with open('D:/Python обучение/Lessons/builds.yaml') as fh:
-    read_data = yaml.load(fh, Loader=yaml.FullLoader)
-print(read_data)
 
-with open('D:/Python обучение/Lessons/tasks.yaml') as fh:
-    read_dat = yaml.load(fh, Loader=yaml.FullLoader)
-# print(read_dat)
+def get_build_tasks(
+        build_name: str,
+        builds: dict[str, list[dict[str, str | list[str]]]],
+        tasks: dict[str, list[dict[str, str | list[str]]]]
+) -> list[str]:
+    build_tasks = []
+    for build in builds.get("builds"):  # type: dict
+        if build.get("name") == build_name:
+            for build_task in build.get("tasks"):
+                build_tasks.extend(
+                    task_subtasks(task_name=build_task, tasks=tasks)
+                )
+            break
+    return build_tasks
